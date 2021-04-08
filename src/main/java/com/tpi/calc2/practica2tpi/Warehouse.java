166c01d7d6d7fa331747f6a3691da8c1411b77e8
cmargueiz <c.margueiz@gmail.com>
2021-04-08 14:39:47 -0600

    inicio

---
+++ src/main/java/com/tpi/calc2/practica2tpi/Warehouse.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tpi.calc2.practica2tpi;
+
+/**
+ *
+ * @author cmargueiz
+ */
+public class Warehouse {
+    
+}
