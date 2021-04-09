20024ab7185ac77105b6c8a1bf04dd37f6f72453
cmargueiz <c.margueiz@gmail.com>
2021-04-08 22:52:22 -0600

    Container y prueba, prueba de package

--- src/main/java/com/tpi/calc2/practica2tpi/Container.java
+++ src/main/java/com/tpi/calc2/practica2tpi/Container.java
@@ -5,10 +5,46 @@
  */
 package com.tpi.calc2.practica2tpi;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author cmargueiz
  */
 public class Container {
-    
+
+    List<Package> contents;
+
+    public Container() {
+        contents = new LinkedList<Package>();
+    }
+
+    public boolean add(Package b) {
+        return !contents.contains(b) && contents.add(b);
+    }
+
+    public boolean remove(Package b) {
+        return contents.remove(b);
+    }
+
+    public double getVolume() {
+        int sum = 0;
+        for (Package b : contents) {
+            sum += b.getVolume();
+        }
+        return sum;
+    }
+
+    public int size() {
+        return contents.size();
+    }
+
+    public void clear() {
+        contents.clear();
+    }
+
+    public boolean contains(Package b) {
+        return contents.contains(b);
+    }
 }
---
+++ src/test/java/com/tpi/calc2/practica2tpi/PackageTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tpi.calc2.practica2tpi;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author cmargueiz
+ */
+public class PackageTest extends TestCase {
+
+    public PackageTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * * Test of getVolume method, of class Package.
+     */
+    public void testGetVolume() {
+        System.out.println("getVolume");
+        Package instance = new Package(1.0);
+        double expResult = 1.0;
+        double result = instance.getVolume();
+        assertEquals(expResult, result);
+    }
+
+}
